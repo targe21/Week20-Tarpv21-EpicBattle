49144e3e38420705f1747d0e50b957365be9d693
targe21 <89987586+targe21@users.noreply.github.com>
2022-01-11 09:09:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EpicBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] heroes = { "Harry Potter", "Superman", "Luke Skywalker", "Lara Croft"};
+            string[] villains = { "Voldemort", "Joker", "Venom", "Darth Vader", "Cruella" };
+
+
+            string randomHero = GetRandomCharacter(heroes);
+            string randomVillain = GetRandomCharacter(villains);
+            Console.WriteLine($"Your random hero is: {randomHero}");
+            Console.WriteLine($"Your random villain is: {randomVillain}");
+        }
+
+        public static string GetRandomCharacter(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomCharacter = someArray[randomIndex];
+            return randomCharacter;
+        }
+    }
+}
